0b6ea12e77d3ac46eba9e2cf89766f8105a258ad
Devin Carlson <devin@devincarlson.ca>
2012-12-31 09:40:33 -0500

    Issue #1875786 by Devin Carlson: Added a  variable and renamed  to .

--- twitter-block-tweet.tpl.php
+++ twitter-block-tweet.tpl.php
@@ -7,7 +7,8 @@
  * A list of tweets are displayed inside of the twitter block.
  *
  * Available variables:
- * - $user_name: The screen name of the tweet's author.
+ * - $name: The name of the tweet's author.
+ * - $screen_name: The screen name of the tweet's author.
  * - $user_image: A URL to the tweet's author's profile picture.
  * - $date: The tweet's creation date.
  * - $text: The tweet's text.
@@ -15,9 +16,9 @@
 ?>
 <div class="clearfix tweet twitter_block">
   <div class="twitter_block_user">
-    <a class="twitter_block profile_image" href="http://twitter.com/<?php echo $user_name; ?>">
+    <a class="twitter_block profile_image" href="http://twitter.com/<?php echo $screen_name; ?>">
       <img src="<?php echo $user_image; ?>" alt="Twitter Avatar" />
-      <span class="twitter_block_user_name"><?php echo $user_name; ?></span>
+      <span class="twitter_block_user_name"><?php echo $name; ?></span>
     </a>
   </div>
   <div class="tweet_date"><?php echo $date; ?></div>
